6823610277b3e7f3a998ef6795e1e44b80b00ce9
TKCSeow <tkcseow@gmail.com>
2022-10-10 12:17:50 +0100

    add error handling for invalid/non exsistant endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,6 +12,17 @@ beforeEach(() => {
     return seed(testData)
 })
 
+describe("ALL *", () => {
+    test("Returns 404 and an error message when non existant endpoint requested", () => {
+      return request(app)
+      .get("/api/nonexistant")
+      .expect(404)
+      .then(({body}) => {
+        expect(body.msg).toBe("404 Route Not Found")
+      })
+    })
+  })
+
 describe('GET /api/topics', () => {
     test("Return status 200 and all topics", () => {
       return request(app)
--- app.js
+++ app.js
@@ -5,5 +5,22 @@ const topicsController = require("./controllers/topics.controller.js")
 
 app.get("/api/topics", topicsController.getTopics)
 
+app.all("*", (req, res) => {
+    res.status(404).send({msg: "404 Route Not Found"})
+})
+
+app.use((err, req, res, next) => {
+    if (err.status && err.msg)
+    {
+        res.status(err.status).send({msg: err.msg})
+    }else{
+        next(err)
+    }
+    
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).send({msg: "500 Bad Server"})  
+})
 
 module.exports = app
